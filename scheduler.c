373e01f4524fd2a320664f4652e9f8bba9286d1f
Ashraf-Bahy <ashrafbahy286@gmail.com>
2024-04-17 02:28:58 +0200

    scheduler: process struct and imported queues

--- scheduler.c
+++ scheduler.c
@@ -1,12 +1,39 @@
 #include "headers.h"
+#include "Queue.h"
 
+typedef struct Process
+{
+    int id;
+    int priority;
+    int arrivalTime;
+    int runTime;
+    int executionTime;
+    int remainingTime;
+    int waitingTime;
+    int state; // 0: waiting, 1: ready, 2: running 
+} Process;
+
+void switchProcess(Process p);
 
 int main(int argc, char * argv[])
 {
     initClk();
     
     //TODO implement the scheduler :)
+    int numberOfProcesses = 100;
+    Process staticProcess = (Process){0, 0, 0, 0, 0, 0, 0}; // this is temporary for now as we dont have input file yet
+    Process* runningProcess = &staticProcess;
+    Process** PCBTable = malloc(numberOfProcesses * sizeof(Process*));
+
+
     //upon termination release the clock resources.
     
     destroyClk(true);
+}
+
+void switchProcess(Process* p)
+{
+    runningProcess->state = 1;
+    runningProcess = p;
+    runningProcess->state = 2;
 }
