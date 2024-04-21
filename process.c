be5856793886bfdae4f7278bb13844fafff5170c
Adham-Hussin <adhamhusn@gmail.com>
2024-04-21 22:00:10 +0200

    Refactor main loop to receive and update remaining time from parent process (scheduler)

--- process.c
+++ process.c
@@ -2,20 +2,41 @@
 
 /* Modify this file as needed*/
 int remainingtime;
+bool var;
 
+void start(int signum);
+void stop(int signum);
 int main(int agrc, char * argv[])
 {
     initClk();
-    
+    signal(SIGUSR1, start);
+    signal(SIGUSR2, stop);
     //TODO it needs to get the remaining time from somewhere
     remainingtime = atoi(argv[1]);
+    int time = -1; 
+    int temp;
+    var = true;
     while (remainingtime > 0)
     {
-        pause(); //waiting for the signal from the parent (scheduler) to decrement the remaining time
-        remainingtime--;
+        temp = getClk();
+        if(var && temp != time)
+        {
+            time = temp;
+            remainingtime--;
+        }
     }
     
     destroyClk(false);
     
     return 0;
+}
+
+void start(int signum)
+{
+    var = true;
+}
+
+void stop(int signum)
+{
+    var = false;
 }
